669e2de1172378b39a86ed87bcc0a84b76ed7de2
erivera1802 <negro6c27@gmail.com>
2026-01-07 00:39:36 +0000

    Deploying to gh-pages from @ erivera1802/erivera1802.github.io@2f2fe43200c0b8fdf3620e5a62b75ce0bca622ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%65%73.%72%69%76%65%72%61%31%32%30%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
