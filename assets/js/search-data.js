45f8199d159ae02731b46f8271eb06316e2b52c6
erivera1802 <negro6c27@gmail.com>
2026-01-07 01:26:51 +0000

    Deploying to gh-pages from @ erivera1802/erivera1802.github.io@b7519b6d1852fedc3585afc8388fec0ffd283b1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,32 +511,32 @@ ninja.data = [{
           window.open("mailto:%65%73.%72%69%76%65%72%61%31%32%30%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/erivera1802", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://instagram.com/este_bandido12", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/estebanrivera1802", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=W1CvMSUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
